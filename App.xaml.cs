f73547dbd2132d59b2d657a18cbf529cc1212c05
Elena Puranu <elena.puranu2@gmail.com>
2023-11-27 18:20:00 +0200

    lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,26 @@
-﻿namespace Puranu_Elena_Lab7;
+﻿using System;
+using Puranu_Elena_Lab7.Data;
+using System.IO;
+
+namespace Puranu_Elena_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+    public App()
 	{
 		InitializeComponent();
 
